93ffe7dbc1b61899a31a9cb26977ad95d1472a07
Jaytee Padilla <padilla.jaytee@gmail.com>
2019-05-31 09:33:13 -0700

    Answered questions in README and Answers files. Project initialized

---
+++ starwars/src/components/starwarsChar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './StarWars.css';
+
+class StarWarsChar extends Component {
+    render() {
+        return (
+            console.log(this.props)
+        )
+    }
+}
+
+export default StarWarsChar;
---
+++ starwars/src/components/starwarsCharList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import StarWarsChar from './starwarsChar';
+import './StarWars.css';
+
+class StarWarsCharList extends Component {
+    render() {
+        return (
+            console.log(this.props)
+        )
+    }
+}
+
+export default StarWarsCharList;
